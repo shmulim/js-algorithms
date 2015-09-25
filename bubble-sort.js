c926bce763a34d3d5df5a01d5a4427e36bf772a8
@shmulim <shmuel.markel@gmail.com>
2015-09-25 13:17:34 -0700

    Update bubble-sort.js

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,25 +1,24 @@
-Array.prototype.bubbleSort = function() {
+function bubbleSort(arr) {
   var switched;
   var temp;
 
   do {
     switched = false;
 
-    for (var i = 0; i < this.length - 1; i++) {
-      if (this[i] > this[i + 1]) {
-        temp = this[i + 1];
-        this[i + 1] = this[i];
-        this[i] = temp;
+    for (var i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        temp = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temp;
         switched = true;
       }
     }
 
   } while (switched);
 
-  return this;
+  return arr;
 }
 
 // implementation
-var array = [3, 2, 1];
-console.log(array.bubbleSort());
+console.log(bubbleSort([3, 2, 1]));
 // outputs: [1, 2, 3]
